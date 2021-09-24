b57d04b0af307491c4b548e606b6731c801be9ae
thirstydeveloper <thirstydeveloper@gmail.com>
2021-09-24 12:01:31 -0400

    Use verification stack output

--- examples/complete/verify/outputs.tf
+++ examples/complete/verify/outputs.tf
@@ -1,3 +1,4 @@
-output "example_bucket" {
-  value = data.aws_s3_bucket.example
+# We can use the verify stack to expose outputs only needed for testing purposes
+output "bucket_id" {
+  value = data.aws_s3_bucket.example.id
 }
--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -21,8 +21,9 @@ func TestExamplesComplete(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		TerraformBinary: "terragrunt",
-		TerraformDir:    "../../examples/complete",
-		Upgrade:         true,
+		// Use the verify stack as the target so we can access its outputs
+		TerraformDir: "../../examples/complete/verify",
+		Upgrade:      true,
 		// These vars are used by the fixture only. We cannot pass them to
 		// vars because then our stack under test will fail complaining
 		// about -var CLI arguments for undeclared variables. We can't use
@@ -35,6 +36,11 @@ func TestExamplesComplete(t *testing.T) {
 		EnvVars: map[string]string{
 			"TF_VAR_fixture_namespace":   namespace,
 			"TF_VAR_fixture_environment": environment,
+
+			// Since we are specifying the verify stack as the terragrunt dir,
+			// we need to include its dependencies so the fixture and stack
+			// code is run.
+			"TERRAGRUNT_INCLUDE_EXTERNAL_DEPENDENCIES": "true",
 		},
 		VarFiles: []string{},
 		Vars:     map[string]interface{}{},
