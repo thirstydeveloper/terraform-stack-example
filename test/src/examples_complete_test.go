61002e7bf410a3b4bcf2b0d9bbb1026400216498
thirstydeveloper <thirstydeveloper@gmail.com>
2021-09-24 13:05:05 -0400

    Rename

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -21,8 +21,8 @@ func TestExamplesComplete(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		TerraformBinary: "terragrunt",
-		// Use the verify stack as the target so we can access its outputs TerraformDir: "../../examples/complete/verify",
-		TerraformDir: "../../examples/complete/verify",
+		// Use the consumer stack as the target so we can access its outputs TerraformDir: "../../examples/complete/consumer",
+		TerraformDir: "../../examples/complete/consumer",
 		Upgrade:      true,
 		// These vars are used by the fixture only. We cannot pass them to
 		// vars because then our stack under test will fail complaining
@@ -37,7 +37,7 @@ func TestExamplesComplete(t *testing.T) {
 			"TF_VAR_fixture_namespace":   namespace,
 			"TF_VAR_fixture_environment": environment,
 
-			// Since we are specifying the verify stack as the terragrunt dir,
+			// Since we are specifying the consumer stack as the terragrunt dir,
 			// we need to include its dependencies so the fixture and stack
 			// code is run.
 			"TERRAGRUNT_INCLUDE_EXTERNAL_DEPENDENCIES": "true",
